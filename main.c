f0ab5b091d7a5fa0671c138c317a7a59f243d0f7
Ivan Dolinsky <ivandolinsky2001@gmail.com>
2017-07-20 00:48:38 +0500

    1

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void)
+{
+	float a, b, c;
+	printf ("Input a=");
+	scanf ("%f", &a);
+	printf ("Input b=");
+	scanf ("%f", &b);
+	c=a/b;
+	printf ("Result c=%f\n", c);
+	return 0;
+
+	
+}
