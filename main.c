cf16ef57425bd0588285f8b03e0b63d522f3f596
Ivan Dolinsky <ivandolinsky2001@gmail.com>
2017-07-21 00:12:52 +0500

    3

--- main.c
+++ main.c
@@ -1,12 +1,14 @@
-#include <stdio.h>
-int main(void)
+ÿ#include <stdio.h>
+int main()
 {
- int a, b, c;
+ int a, b, c, d, e;
 	printf ("Input a=");
 	scanf("%d", &a);
 	printf ("Input b=");
 	scanf("%d", &b);
-	c=a%b;
-		printf("Result c=%d\n", c);
+	c=a*b;
+	d=a+b;
+	e=c%d;
+		printf("Result e=%d\n", e);
 return 0;	
 }
