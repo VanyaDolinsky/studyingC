7be169b25dc41a218bdd0dc3503d65848e624917
Ivan Dolinsky <ivandolinsky2001@gmail.com>
2017-07-21 01:43:15 +0500

    5

--- main.c
+++ main.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 int main()
 {
-	int a, c, d, i;
+	float a, b, c, d, e, f, g, h, m;
 	printf ("Input a=");
-	scanf("%4d", &a);
-	c=0;
-	for (i=0; i<4;i++)
-	{
-		d=a%10;
-		c=c+d;
-		a=a/10;
-	}
-	printf("Result c=%4d\n", c);
+	scanf("%f", &a);
+	printf ("Input b=");
+	scanf("%f", &b);
+	printf ("Input c=");
+	scanf("%f", &c);
+	printf ("Input d=");
+	scanf("%f", &d);
+	e=1;
+	f=-(a+b+c+d);
+	g=a*b+a*c+a*d+b*c+b*d+c*d;
+	h=-(a*b*c+a*b*d+a*c*d+b*c*d);
+	m=a*b*c*d;
+	printf("Result\n e= %f\n f=%f\n g= %f\n h=%f\n m= %f\n", e, f, g, h, m);
 	return 0;	
 }
