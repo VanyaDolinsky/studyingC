0e1c6bb5cd34fc3c76d492af47fcb16bc5dfa159
Ivan Dolinsky <ivandolinsky2001@gmail.com>
2017-07-22 00:53:34 +0500

    6

--- main.c
+++ main.c
@@ -1,20 +1,16 @@
 #include <stdio.h>
 int main()
 {
-	float a, b, c, d, e, f, g, h, m;
-	printf ("Input a=");
-	scanf("%f", &a);
-	printf ("Input b=");
-	scanf("%f", &b);
-	printf ("Input c=");
-	scanf("%f", &c);
-	printf ("Input d=");
-	scanf("%f", &d);
-	e=1;
-	f=-(a+b+c+d);
-	g=a*b+a*c+a*d+b*c+b*d+c*d;
-	h=-(a*b*c+a*b*d+a*c*d+b*c*d);
-	m=a*b*c*d;
-	printf("Result\n e= %f\n f=%f\n g= %f\n h=%f\n m= %f\n", e, f, g, h, m);
+	int a, b;
+	printf("Input a=");
+	scanf("%d",&a);
+	printf("Input b=");
+	scanf("%d", &b);
+	if(a>b)
+		if (a=b)
+		 printf("a\n");
+			else
+		printf("b\n");
+		printf("");
 	return 0;	
 }
