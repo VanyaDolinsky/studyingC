25bf32ad221d15d98dae2221740ece7095292f86
Ivan Dolinsky <ivandolinsky2001@gmail.com>
2017-07-22 14:25:23 +0500

    fixed

--- main.c
+++ main.c
@@ -4,12 +4,7 @@ int main()
 	int a;
 	printf("Input a=");
 	scanf("%d",&a);
-	if (a>0)
-		if (a%2==0)
+	if (a>0&&a%2==0)
 		 printf("all right/n");
-			else
-			 printf("");
-	else
-			printf("");
 	return 0;	
 }
