6267dd62c3a00b7c4b0b9051ed4589c993e77dc1
Ivan Dolinsky <ivandolinsky2001@gmail.com>
2017-07-22 13:33:47 +0500

    fixed

--- main.c
+++ main.c
@@ -6,11 +6,12 @@ int main()
 	scanf("%d",&a);
 	printf("Input b=");
 	scanf("%d", &b);
-	if(a>b)
-		if (a=b)
+	if(a!=b)
+		if (a>b)
 		 printf("a\n");
 			else
 		printf("b\n");
+			else
 		printf("");
 	return 0;	
 }
