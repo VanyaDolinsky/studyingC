c95f47607ed86a005c5c637a82888ab6ebf36c0a
Ivan Dolinsky <ivandolinsky2001@gmail.com>
2017-07-24 00:37:55 +0500

    fixed

--- main.c
+++ main.c
@@ -6,7 +6,9 @@ int main()
 	scanf("%f", &x);
 	printf("Input y=");
 	scanf("%f", &y);
-	if((x<0 && x*x+y*y<=200*200 && y>x && y<-x) || (x>=0 && x*x+y*y>=200*200 && x<200 && y>-200 && y<200))
+	if((x<=0 && x*x+y*y<=200*200 && y>=-x && y<=-x) || (x>=0 && x*x+y*y>=200*200 && x<=200 && y>=-200 && y<=200))
 		printf("all right\n");
+	else
+		printf("doesn't own");
 	return 0;	
 }
