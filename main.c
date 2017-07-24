093bfbc1f962fed8fb15b701715b26c3c7711635
Ivan Dolinsky <ivandolinsky2001@gmail.com>
2017-07-25 01:59:23 +0500

    13

--- main.c
+++ main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 int main()
 {
-
+	int n, k, s, h;
+	h=1;
+	printf("Input n=");
+	scanf("%d", &n);
+	printf("Input k=");
+	scanf("%d", &k);
+	if (k==0)
+	{
+		printf("1\n");
+	}
+	else
+	{
+		for(s=1; s<=k; s++)
+		{
+			h=h*n;
+		}
+		printf("Result h=%d\n", h);
+	}		
 	return 0;	
 }
