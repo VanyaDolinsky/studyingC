268790cf93a0e2952c6bef1ec4e249a58f2febd1
Ivan Dolinsky <ivandolinsky2001@gmail.com>
2017-07-25 02:54:22 +0500

    ашчув

--- main.c
+++ main.c
@@ -6,10 +6,11 @@ int main()
 	scanf("%d", &a);
 	printf("Input b=");
 	scanf("%d", &b);
-	a>0;
-	b>0;
-	c=a+b;
-	if(a>0 && b>0)
-		printf("Result c=%d\n", c);
+	c=0;
+	while (a>0 && b>0)
+	{
+		c+=a+b;
+	}
+	printf("Result c=%d\n", c);
 	return 0;	
 }
