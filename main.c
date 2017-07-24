10b621b319011ae54e1b256bdb1a67db24686d68
Ivan Dolinsky <ivandolinsky2001@gmail.com>
2017-07-25 00:36:11 +0500

    -12

--- main.c
+++ main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 int main()
 {
-
+	int a, b, c;
+	printf("Input a=");
+	scanf("%d", &a);
+	printf("Input b=");
+	scanf("%d", &b);
+	a>0;
+	b>0;
+	c=a+b;
+	if(a>0 && b>0)
+		printf("Result c=%d\n", c);
 	return 0;	
 }
