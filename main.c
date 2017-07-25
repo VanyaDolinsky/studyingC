a876948a78ec42b8dc48b3cd0c1999ec89a267e9
Ivan Dolinsky <ivandolinsky2001@gmail.com>
2017-07-26 01:50:39 +0500

    15

--- main.c
+++ main.c
@@ -1,10 +1,32 @@
 #include <stdio.h>
 int main()
 {
-	int a;
+	int a; // число простое res = 1; число не простое res = 0
+	
 	printf("Input a=");
 	scanf("%d", &a);
-	if(a%2>0 && a%3>0 && a>0)
-		printf("all right\n");
+
+	if(a==2)
+	{
+		printf("easy\n");
+	}
+
+	for (int i=2; i<a; i++)
+	{
+		if (a%i==0)
+		{
+			printf("hard\n");
+			break;
+		}
+
+
+		if (i==a-1)
+		{
+			printf("easy\n");
+		}
+	
+	}
+
+	
 	return 0;	
 }
