cbab73392c235307b16f1f599465496bb92ead1f
Ivan Dolinsky <ivandolinsky2001@gmail.com>
2017-07-26 23:35:25 +0500

    15

--- main.c
+++ main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 int main()
 {
+	int k, res=1;
+	float m;
+	printf("Input m=");
+	scanf("%f", &m);
+	for (k=0; res<m; k++)
+	{
+	res*=3;
+	}
+	printf("Result k=%d\n", k);
 
 	return 0;	
 }
