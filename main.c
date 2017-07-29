a715caec45b9f5a58d8e10ada61d04e85d1b2d32
Ivan Dolinsky <ivandolinsky2001@gmail.com>
2017-07-29 14:58:57 +0500

    17

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 int main()
 {
-
+	int n, i, f;
+	printf("Input n=");
+	scanf("%d", &n);
+	for(i=1; i<=n; i++)
+	{
+		for (f=1; f<=i; f++)
+		{
+			printf("%d ", i);
+		}
+		printf("\n");
+	}
 	return 0;	
 }
