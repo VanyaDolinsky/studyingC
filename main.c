6fa730124f73911e5df44d1cda1e92e9ff210f3f
Ivan Dolinsky <ivandolinsky2001@gmail.com>
2017-07-31 01:51:40 +0500

    fixed

--- main.c
+++ main.c
@@ -1,16 +1,19 @@
-#include <stdio.h>
+û#include <stdio.h>
 int main()
 {
 	int f, m, c;
 	c=0;
-	 for(m=190; m<210; m++)
+	for(m=190; m<210; m++)
 	{
 		for (f=2; f<m; f++)
 		{
-			if(m%f==0)
-				c+=1;
-			printf(" %d\n", " %d\n", " %d\n", m, f, c);
-
+			c=0;
+			if (m%f==0)
+			{
+				c++;
+				printf(" chislo %d deliteli %d kolichestvo %d\n", m, f, c);
+				
+			}
 		}
 	}
 	return 0;	
