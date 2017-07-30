e4b3752f197f02e0c93d1384e0c1c8f339fdf637
Ivan Dolinsky <ivandolinsky2001@gmail.com>
2017-07-31 02:11:28 +0500

    fixed1

--- main.c
+++ main.c
@@ -3,6 +3,7 @@ int main()
 {
 	int f, m, c;
 	c=0;
+	printf("chislo deliteli kolichestvo\n");
 	for(m=190; m<210; m++)
 	{
 		for (f=2; f<m; f++)
@@ -11,8 +12,11 @@ int main()
 			if (m%f==0)
 			{
 				c++;
-				printf(" chislo %d deliteli %d kolichestvo %d\n", m, f, c);
-				
+				printf("%d   %d %d %d %d \n", m, f, f, f, f, c);
+			}
+			else
+			{
+				printf("%d", m);	
 			}
 		}
 	}
