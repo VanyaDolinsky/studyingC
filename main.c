da892cf6a030159c2062170fd1301000a8acb6ea
Ivan Dolinsky <ivandolinsky2001@gmail.com>
2017-08-01 01:55:11 +0500

    fixed6

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 int main()
 {
-
+	int n, i, sum, c, k, h;
+	printf("Input n=");
+	scanf("%d", &n);
+	for (i=1; i<=n; i++);
+	{
+		for (c=1; c<=n; c++)
+		{
+			k=c*i;
+			h=i;
+			h*=k;
+			sum+=h;
+			printf("%d\n", sum);
+			}
+		}
+	}
 	return 0;	
 }
