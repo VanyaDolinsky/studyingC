877f07e5cd984f54f92e5166f80d20bd60b904a4
Ivan Dolinsky <ivandolinsky2001@gmail.com>
2017-08-06 13:06:28 +0500

    fixed1

--- main.c
+++ main.c
@@ -6,11 +6,13 @@ int main()
 	double sum=0.0;
 	for(N=800; N<=1000; N+=100)
 	{
+		sum=0;
 		for(n=1; n<=N; n++)   
 		{
 			sum+=exp(sqrt(n));
 		}
+			printf("N=%d\n", N);
+			printf("sum=%f\n", sum);
 	}
-	printf("%f\n", sum);
 	return 0;	
 }
