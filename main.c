68b1dfe4231c1b76dd0619b28a39114b89dbe404
Ivan Dolinsky <ivandolinsky2001@gmail.com>
2017-08-08 02:16:04 +0500

    fixed2

--- main.c
+++ main.c
@@ -1,6 +1,47 @@
 #include <stdio.h>
+#include <math.h>
 int main()
 {
+	double	 n, i, k;
+	double sum=0.0;
+	for (n=1000; n<=5000; n*=5)
+	{
+		sum=0;
+		for(i=1; i<n; i++)
+		{
+			sum+=i/(n*n);
+		}
+		printf("%f\n", sum);
+	}
+
+	sum=0;
+	n=10000;
+	for(i=1; i<10000; i++)
+	{
+		sum+=i/(n*n);
+	}
+	printf("%f\n", sum);
+	k=n/10000;
+
+	n = 1;
+	sum=0;
+	double sum_prev=0;
+	k=1;
+	while(k>0.0001)
+	{
+		sum = 0;
+		for(int i = 1; i < n; i++)
+		{
+			sum+=(double)i/(i*i);
+		}
+		printf("sum = %f\n", sum);
+		printf("sum_prev = %f\n", sum_prev);
+		k = sum - sum_prev;
+		sum_prev = sum;
+		n++;
+	}
+	/*printf("%f\n", n);
+	printf("%f\n", sum);*/
 
 	return 0;	
 }
