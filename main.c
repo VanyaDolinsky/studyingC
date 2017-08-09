d160126cf9ff4531856f16b57a707735feeae0a3
Ivan Dolinsky <ivandolinsky2001@gmail.com>
2017-08-10 02:33:20 +0500

    21

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 int main()
 {
+	int a = 5;
+	printf("a = %d\n", a); // выводит значение a
+	printf("ss_a = %d\n", &a); // выводит адрес ячейки в памяти, которая хранит значение a
+	int *b = &a;
+	
+	printf("\n\n"); 
+	
+	*b = 10;
+	printf("a = %d\n", a); 
+	printf("b = %d\n", b);
+	printf("value_b = %d\n", *b);
 
 	return 0;	
 }
