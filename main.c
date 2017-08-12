1cbdf7ceb2d14fdcf5c95231389af6508886abd0
Ivan Dolinsky <ivandolinsky2001@gmail.com>
2017-08-12 20:18:16 +0500

    12

--- main.c
+++ main.c
@@ -3,12 +3,14 @@
 #include <time.h>
 int main()
 {
-	srand( time( NULL ) );
-	int a[15];
+	srand( time(NULL) );
+	int a[15], sum=0;
 	for(int i=0; i<15; i++)
 	{
 		a[i]=rand()% 12 + 1;
+		sum+=a[i];
 		printf("%d ", a[i]); 
 	}
+	printf("\nsum = %d\n", sum); 
 	return 0;	
 }
