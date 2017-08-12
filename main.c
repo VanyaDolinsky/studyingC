39c1501c119542a047e2a93d1d13519da2b13c21
Ivan Dolinsky <ivandolinsky2001@gmail.com>
2017-08-13 01:36:03 +0500

    121212

--- main.c
+++ main.c
@@ -1,6 +1,25 @@
-#include <stdio.h>
-int main()
-{
-
-	return 0;	
-}
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <time.h>
+  int main()
+  {
+	srand( time( NULL ) );
+	int a[15], max;
+	srand( time(NULL) );
+  	for(int i=0; i<15; i++)
+  	{
+  		a[i]=rand()% 12 + 1;
+		printf("%d ", a[i]);
+  	}
+	max=a[1];
+	for(int i=0; i<15; i++)
+  	{
+		if(max<a[i])
+		{
+			max=a[i];
+		}
+  	}
+	
+	printf("\n max= %d\n", max); 
+  	return 0;	
+  }
