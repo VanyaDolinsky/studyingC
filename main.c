67be30a08336ceacabf33433fd692effddba1e1a
Ivan Dolinsky <ivandolinsky2001@gmail.com>
2017-08-14 00:58:35 +0500

    1231214

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include <math.h>
 int main()
 {
-
+	int i=0;
+	double y[41], x;
+	for(x=-6; x<=6; x+=0.3)
+	{
+		y[i]=5*sin(2*x);
+		printf("%f\n", y[i]);
+		i++;
+	}
 	return 0;	
 }
