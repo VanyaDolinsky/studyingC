f7c16f3aced469c06a17ea04c623e7e115ab3750
Ivan Dolinsky <ivandolinsky2001@gmail.com>
2017-08-16 00:07:24 +0500

    1212121212

--- main.c
+++ main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 int main()
 {
-
+	srand( time(NULL) );
+	int a[12], min, max, i;
+	printf("Input min=");
+	scanf("%d", &min);
+	printf("Input max=");
+	scanf("%d", &max);
+	for (i=0; i<11; i++)
+	{
+		a[i]=rand() %(max-min) + min;
+		printf("%d ", a[i]);
+	}
 	return 0;	
 }
