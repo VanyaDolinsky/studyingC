56e67b4e6de060f4ec39cf32666e6ed0d3f2dc14
Ivan Dolinsky <ivandolinsky2001@gmail.com>
2017-08-16 00:55:56 +0500

    1212123443

--- main.c
+++ main.c
@@ -4,15 +4,36 @@
 int main()
 {
 	srand( time(NULL) );
-	int a[12], min, max, i;
+	int k[12], min, max, i, a, sum=0, e=0, e1=0;
+	double  m;
 	printf("Input min=");
 	scanf("%d", &min);
 	printf("Input max=");
 	scanf("%d", &max);
+	printf("Input a=");
+	scanf("%d", &a);
 	for (i=0; i<11; i++)
 	{
-		a[i]=rand() %(max-min) + min;
-		printf("%d ", a[i]);
+		k[i]=rand() %(max-min) + min;
+		printf("%d ", k[i]);
 	}
+	for (i=0; i<11; i++)
+	{
+		if(a<k[i])
+		{
+		sum+=k[i];
+		e++;
+		}
+	}
+	m=sum/e;
+	printf("m=%f ", m);
+	for (i=0; i<11; i++)
+	{
+		if(m<k[i])
+		{
+		e1++;
+		}
+	}
+	printf("e1=%d", e1);
 	return 0;	
 }
