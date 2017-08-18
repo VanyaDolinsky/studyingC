6fadca303e04f402c0972e87e53fdbe812ff2cca
Ivan Dolinsky <ivandolinsky2001@gmail.com>
2017-08-19 01:35:57 +0500

    12121212121212124564564

--- main.c
+++ main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+double InchToM(double x);
 int main()
 {
-
+	float a, m;
+	printf("Input inch=");
+	scanf("%f", &a);
+	m=InchToM(a);
+	printf("%f\n", m);
 	return 0;	
+}
+
+double InchToM(double x)
+{
+	double y;
+	y=x*0.0254;
+	return y;
 }
