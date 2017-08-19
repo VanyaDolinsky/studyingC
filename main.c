402bb971a08fdd463a6aaf551153cc10b879c794
Ivan Dolinsky <ivandolinsky2001@gmail.com>
2017-08-19 23:02:15 +0500

    323232442332423

--- main.c
+++ main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include <math.h>
+void func(float a, float b, float c, float *x1, float *x2);
+
 int main()
 {
-
+	//float a, b, c, x1, x2;
+	float a, b, c, x1, x2;
+	printf("Input a=");
+	scanf("%f", &a);
+	printf("Input b=");
+	scanf("%f", &b);
+	printf("Input c=");
+	scanf("%f", &c);
+	func(a, b, c, &x1, &x2);
+	printf("%f\n%f\n", x1, x2);
 	return 0;	
+}
+
+void func(float a, float b, float c, float *x1, float *x2)
+{
+	*x1=(-b+sqrt(b*b-4*a*c))/2*a;
+	*x2=(-b-sqrt(b*b-4*a*c))/2*a;
 }
