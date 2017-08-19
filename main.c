813e1b5444d9361964cf67ea5e26b30e9a85c8b2
Ivan Dolinsky <ivandolinsky2001@gmail.com>
2017-08-19 23:31:43 +0500

    32323244233242356565

--- main.c
+++ main.c
@@ -4,7 +4,6 @@ void func(float a, float b, float c, float *x1, float *x2);
 
 int main()
 {
-	//float a, b, c, x1, x2;
 	float a, b, c, x1, x2;
 	printf("Input a=");
 	scanf("%f", &a);
@@ -13,7 +12,10 @@ int main()
 	printf("Input c=");
 	scanf("%f", &c);
 	func(a, b, c, &x1, &x2);
+	if ((b*b-4*a*c)>0)
 	printf("%f\n%f\n", x1, x2);
+	if ((b*b-4*a*c)==0)
+	printf("%f\n", x1);
 	return 0;	
 }
 
@@ -21,4 +23,14 @@ void func(float a, float b, float c, float *x1, float *x2)
 {
 	*x1=(-b+sqrt(b*b-4*a*c))/2*a;
 	*x2=(-b-sqrt(b*b-4*a*c))/2*a;
+	if((b*b-4*a*c)==0)
+	{
+		printf("kvadrat\n");
+	}
+	if((b*b-4*a*c)<0)
+	{
+		printf("netkorney\n");
+	}
+
+
 }
