95ba74bae3433ad1fa4f198d2320dde44c3e8fd6
Ivan Dolinsky <ivandolinsky2001@gmail.com>
2017-08-24 02:19:42 +0500

    32323244233242356565

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
-int main()
-{
-
-	return 0;	
+#define BASE 10
+long int BASEto3(long int a);
+int main(void) {
+	long int x, y;
+	printf("a=");
+	scanf("%ld", &x);
+	y = BASEto3(x);
+	printf("x=%ld\n", y);
+	return 0;
+}
+long int BASEto3(long int a) {
+	int k=1;
+	long int a3=0;
+	while (a) {
+		a3 += k*(a%3);
+		k *= BASE;
+		a/=3;
+	}
+return a3;
 }
