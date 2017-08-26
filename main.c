a8900cc27e1125e4108d940fabfc9a864e14cbff
Ivan Dolinsky <ivandolinsky2001@gmail.com>
2017-08-26 22:16:08 +0500

    32323244233242356565

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
-int main()
-{
-
-	return 0;	
+#include <math.h>
+ 
+long int fact(int i);
+int main(void) {
+	int n, a;
+	printf("Input n:");
+	scanf("%d", &n);
+	printf("Input a:");
+	scanf("%d", &a);
+	printf("Factorial n!=%ld\n", fact(n));
+return 0;
+}
+	long int fact(int i) 
+	{
+	}
 }
