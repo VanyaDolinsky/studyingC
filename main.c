802298e2564e7622807403b77aa1f4f3481a2fbb
Ivan Dolinsky <ivandolinsky2001@gmail.com>
2017-08-28 02:46:22 +0500

    32323244233242356565

--- main.c
+++ main.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 #include <math.h>
- 
-long int fact(int i);
+float fact(float *, float *);
 int main(void) {
-	int n, a;
-	printf("Input n:");
-	scanf("%d", &n);
+	float n, a;
+	float k;
 	printf("Input a:");
-	scanf("%d", &a);
-	printf("Factorial n!=%ld\n", fact(n));
+	scanf("%f", &a);
+	printf("Input n:");
+	scanf("%f", &n);
+	k=fact(&a, &n);
+	printf("Factorial k=%f\n", k);
 return 0;
 }
-	long int fact(int i) 
-	{
-	}
+float fact(float *i, float *n) {
+	if (n==0)
+		return 1;
+	else
+		n--;
+		return fact(sqrt(*i+fact(*i)));
 }
