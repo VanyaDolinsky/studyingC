17d8533a251c7675de5d534767761e298a928128
Ivan Dolinsky <ivandolinsky2001@gmail.com>
2017-09-01 01:48:09 +0500

    32323244233242356565787870989889

--- main.c
+++ main.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
-int main()
+int func(int *, int );
+int main(void)
 {
-
+	int n, m, k;
+	printf("Input n=");
+	scanf("%d", &n);
+	printf("Input m=");
+	scanf("%d", &m);
+	k=func(&n, m);
+	printf("k=%d\n", k);
 	return 0;	
 }
+int func(int *n, int m)
+{
+	int k=*n;
+	if (k==0)
+	{
+		return 1;
+	}	
+	if(k<m)
+	{
+		return -1;
+	}
+	int s=k-1;
+	return 2*func(&s, m);
+}
+
+	
