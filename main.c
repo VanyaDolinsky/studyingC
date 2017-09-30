48b8a75f4b3087b05e245f5e5626dfd2b2d0ae04
Ivan Dolinsky <ivandolinsky2001@gmail.com>
2017-10-01 01:00:29 +0500

    random1

--- main.c
+++ main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
-int main()
+#include <stdlib.h>
+#include <time.h>
+void
+elem2 (int a[], int size)
 {
+	srand( time( NULL ) );
+    for(int i=0; i<size; i++)
+	{
+		a[i]=rand()% 12 + 1;
+	}
+}
 
-	return 0;	
+int main ()
+{
+	int a[3] = {1,2,3};
+	elem2(a,3);
+	for (int i=0; i<3; i++)
+	{
+		printf("%d\n",a[i]);
+ 	} 
+	return 0;
 }
