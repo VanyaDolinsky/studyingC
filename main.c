421e320defe6e95c9f133fa97097c6ce0abf564d
Ivan Dolinsky <ivandolinsky2001@gmail.com>
2017-10-03 23:08:15 +0500

    eweqdffds

--- main.c
+++ main.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-void
-elem2 (int a[], int size)
+int FillArray (int *, int);
+int main ()
 {
+	int a[3] = {1,2,3};
+	int k;
+	k=FillArray(a,3);
+	printf("%d\n", k);
+	return 0;
+}
+
+int FillArray (int a[], int size)
+{
+	int s=0;
 	srand( time( NULL ) );
     for(int i=0; i<size; i++)
 	{
 		a[i]=rand()% 12 + 1;
+		s+=a[i];
 	}
-}
-
-int main ()
-{
-	int a[3] = {1,2,3};
-	elem2(a,3);
-	for (int i=0; i<3; i++)
-	{
-		printf("%d\n",a[i]);
- 	} 
-	return 0;
+	return s;
 }
