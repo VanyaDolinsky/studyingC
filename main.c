665723e29950e213af53800a144df56fdb02b04e
Ivan Dolinsky <ivandolinsky2001@gmail.com>
2017-10-13 01:06:16 +0500

    write

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 int main()
 {
-
+	FILE *f;
+	int sum=0, size=0, a[100];
+	f=fopen("input.txt", "r");
+	for (int i=0; !feof(f); i++)
+	{
+		fscanf(f, "%d", &a[i]);
+		size++;
+	}
+	fclose(f);
+	f=fopen("output.txt", "w");
+	for(int i=0; i<size; i++)
+	{
+		sum+=a[i];	
+	}
+	fprintf(f, "%d ", sum);
+	fclose(f);
 	return 0;	
 }
